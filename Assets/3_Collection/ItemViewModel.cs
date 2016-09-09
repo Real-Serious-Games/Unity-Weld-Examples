8b7c51697bba4619852aa0ee90098e046667f4b6
Ashley Davis <ashley.davis@realseriousgames.com>
2016-09-09 13:24:15 +1000

    Added example 3.

---
+++ Assets/3_Collection/ItemViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using UnityUI.Binding;
+
+/// <summary>
+/// View-model for items displayed in the collection.
+/// </summary>
+[Binding]
+public class ItemViewModel
+{
+    [Binding]
+    public string DisplayText
+    {
+        get;
+        private set;
+    }
+
+    public ItemViewModel(string displayText)
+    {
+        this.DisplayText = displayText;
+    }
+}
+
---
+++ Assets/3_Collection/MyUI3.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityUI.Binding;
+using System.ComponentModel;
+using System;
+using UnityEngine.UI;
+
+[Binding]
+public class MyUI3 : MonoBehaviour, INotifyPropertyChanged
+{
+    private ObservableList<ItemViewModel> items = new ObservableList<ItemViewModel>()
+    {
+        new ItemViewModel("1"),
+        new ItemViewModel("2"),
+        new ItemViewModel("3")
+    };
+
+    [Binding]
+    public ObservableList<ItemViewModel> Items
+    {
+        get
+        {
+            return items;
+        }
+    }
+
+
+    /// <summary>
+    /// Event to raise when a property's value has changed.
+    /// </summary>
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnPropertyChanged(string propertyName)
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

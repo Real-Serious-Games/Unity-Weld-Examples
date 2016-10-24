726475420a10df72832420adcb1191a35ea6652f
Ashley Davis <ashley.davis@realseriousgames.com>
2016-10-24 10:14:44 +1000

    3rd example. Simple example of event binding.

---
+++ Assets/3_Binding_a_button_click/MyViewModel3.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityUI.Binding;
+using System;
+
+[Binding]
+public class MyViewModel3 : MonoBehaviour
+{
+    private float cubeRotation = 0f;
+
+    [Binding]
+    public void RotateCube()
+    {
+        cubeRotation = cubeRotation + 10f;
+    }
+
+    void Update ()
+    {
+        var cube = GameObject.Find("Cube");
+        cube.transform.localEulerAngles = new Vector3(0f, cubeRotation, 0f);
+	}
+}

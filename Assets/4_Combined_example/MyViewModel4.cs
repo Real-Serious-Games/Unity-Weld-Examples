1934b6ba9046e52b83cd4ab359f6b1f9a950e668
Ashley Davis <ashley.davis@realseriousgames.com>
2016-10-25 10:08:22 +1000

    Added example 4, which combines the previous 3 examples.

---
+++ Assets/4_Combined_example/MyViewModel4.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityUI.Binding;
+using System;
+using System.ComponentModel;
+
+[Binding]
+public class MyViewModel4 : MonoBehaviour, INotifyPropertyChanged
+{
+    private float cubeRotation = 0f;
+
+    private string text = "0";
+
+    [Binding]
+    public string Text
+    {
+        get
+        {
+            return text;
+        }
+        set
+        {
+            if (text == value)
+            {
+                return; // No change.
+            }
+
+            text = value;
+
+            try
+            {
+                cubeRotation = float.Parse(text);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+
+            OnPropertyChanged("Text");
+        }
+    }
+
+    /// <summary>
+    /// Event to raise when a property's value has changed.
+    /// </summary>
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    [Binding]
+    public void RotateCube()
+    {
+        cubeRotation = cubeRotation + 10f;
+        text = cubeRotation.ToString();
+
+        OnPropertyChanged("Text");
+    }
+
+    void Update()
+    {
+        var cube = GameObject.Find("Cube");
+        cube.transform.localEulerAngles = new Vector3(0f, cubeRotation, 0f);
+    }
+
+    private void OnPropertyChanged(string propertyName)
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

9b0d935bf324bda062fc4153e7f0d4aca4c9b457
Ashley Davis <ashley.davis@realseriousgames.com>
2016-11-02 11:33:44 +1000

    Refactored the code to show the view-model property menu so that it is shared by all editors.

--- Assets/Unity-Weld/UnityUI_Editor/TwoWayPropertyBindingEditor.cs
+++ Assets/Unity-Weld/UnityUI_Editor/TwoWayPropertyBindingEditor.cs
@@ -83,14 +83,13 @@ namespace UnityUI_Editor
                 }
             }
 
-            var bindableViewModelProperties = TypeResolver.FindBindableProperties(targetScript);
-            ShowViewModelPropertySelector(
+            ShowViewModelPropertyMenu(
                 "View-model property",
-                targetScript, 
-                bindableViewModelProperties,
+                targetScript,
+                TypeResolver.FindBindableProperties(targetScript),
                 updatedValue => targetScript.viewModelPropertyName = updatedValue,
                 targetScript.viewModelPropertyName,
-                adaptedViewPropertyType
+                property => property.PropertyType == adaptedViewPropertyType
             );
 
             var viewModelAdapterTypeNames = TypeResolver.TypesWithAdapterAttribute
@@ -131,16 +130,14 @@ namespace UnityUI_Editor
                 }
             }
 
-            var exceptionViewModelProperties = TypeResolver.FindBindableProperties(targetScript);
-            ShowViewModelPropertySelector(
+            ShowViewModelPropertyMenu(
                 "Exception property",
-                targetScript, 
-                exceptionViewModelProperties,
+                targetScript,
+                TypeResolver.FindBindableProperties(targetScript),
                 updatedValue => targetScript.exceptionPropertyName = updatedValue,
                 targetScript.exceptionPropertyName,
-                adaptedExceptionPropertyType
+                property => property.PropertyType == adaptedExceptionPropertyType
             );
-
         }
 
         /// <summary>
@@ -183,38 +180,5 @@ namespace UnityUI_Editor
             );
         }
 
-        private void ShowViewModelPropertySelector(
-            string label,
-            TwoWayPropertyBinding target, 
-            PropertyInfo[] bindableProperties,
-            Action<string> propertyNameSetter,
-            string propertyName,
-            Type viewPropertyType
-        )
-        {
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.PrefixLabel(label);
-
-            var dropdownPosition = GUILayoutUtility.GetLastRect();
-            dropdownPosition.x += dropdownPosition.width;
-
-            if (GUILayout.Button(new GUIContent(propertyName), EditorStyles.popup))
-            {
-                InspectorUtils.ShowMenu<PropertyInfo>(
-                    property => property.ReflectedType + "/" + property.Name + " : " + property.PropertyType.Name,
-                    property => property.PropertyType == viewPropertyType,
-                    property => property.ReflectedType.Name + "." + property.Name == propertyName,
-                    property => UpdateProperty(
-                        propertyNameSetter,
-                        propertyName,
-                        property.ReflectedType.Name + "." + property.Name
-                    ),
-                    bindableProperties,
-                    dropdownPosition
-                );
-            }
-
-            EditorGUILayout.EndHorizontal();
-        }
     }
 }

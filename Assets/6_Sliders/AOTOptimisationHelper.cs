19626859b6c3e34a3881153accd6e62c17c1d0d3
Rory Dungan <rorydungan@gmail.com>
2017-03-18 15:27:14 +1000

    Added class that ensures necessary types aren't removed by IL2CPP
    Fixes issue on Xbox One and WebGL

---
+++ Assets/6_Sliders/AOTOptimisationHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityWeld.Binding.Internal;
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedMember.Local
+// ReSharper disable UnusedVariable
+
+#pragma warning disable 219 // Disable warning that variable is never used
+
+/// <summary>
+/// In order for certain generic types to not be optimised-out by IL2CPP for
+/// platforms like Xbox One, iPhone and WebGL, we need to reference them at 
+/// least once in code instead of just calling them via reflection. 
+/// 
+/// See this page for more details:
+/// https://docs.unity3d.com/Manual/TroubleShootingIPhone.html 
+/// In the section "The game crashes with the error message “ExecutionEngineException: 
+/// Attempting to JIT compile method ‘SometType`1&lt;SomeValueType&gt;:.ctor ()’ while 
+/// running with –aot-only.”"
+/// </summary>
+internal class AOTOptimisationHelper
+{
+    // Even though this method is never called, the fact that it exists will
+    // ensure the compiler includes the types referenced in it so that we can
+    // later refer to those via reflection.
+    private void EnsureGenericTypes()
+    {
+        var strEventBinder = new UnityEventBinder<string>(null, null);
+        var floatEventBinder = new UnityEventBinder<float>(null, null);
+    }
+}
+
+#pragma warning restore 219

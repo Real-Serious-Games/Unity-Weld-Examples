05243b75283bc5b3faaf75c3b1c8b18c9e8c04ea
Rory Dungan <rorydungan@gmail.com>
2017-05-09 10:02:43 +1000

    Added SubViewModelBinding example

---
+++ Assets/SubViewModelBinding/ColorViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityWeld.Binding;
+
+/// <summary>
+/// View model for controlling the color of a mesh.
+/// </summary>
+[Binding]
+public class ColorViewModel
+{
+    private Material mat;
+
+    public ColorViewModel(MeshRenderer mesh)
+    {
+        mat = mesh.material;
+    }
+
+    /// <summary>
+    /// The red component of the mesh's color.
+    /// </summary>
+    [Binding]
+    public float Red
+    {
+        get
+        {
+            return mat.color.r;
+        }
+        set
+        {
+            var color = mat.color;
+            color.r = value;
+            mat.color = color;
+        }
+    }
+
+    /// <summary>
+    /// The green component of the mesh's color.
+    /// </summary>
+    [Binding]
+    public float Green
+    {
+        get
+        {
+            return mat.color.g;
+        }
+        set
+        {
+            var color = mat.color;
+            color.g = value;
+            mat.color = color;
+        }
+    }
+
+    /// <summary>
+    /// The blue component of the mesh's color.
+    /// </summary>
+    [Binding]
+    public float Blue
+    {
+        get
+        {
+            return mat.color.b;
+        }
+        set
+        {
+            var color = mat.color;
+            color.b = value;
+            mat.color = color;
+        }
+    }
+}
---
+++ Assets/SubViewModelBinding/ObjectViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityWeld.Binding;
+
+[Binding]
+public class ObjectViewModel : MonoBehaviour
+{
+    /// <summary>
+    /// The object we want to manipulate.
+    /// </summary>
+    public MeshRenderer obj;
+
+    private ColorViewModel colorViewModel;
+
+    /// <summary>
+    /// Sub-view model for controlling the color of the object.
+    /// </summary>
+    [Binding]
+    public ColorViewModel SubViewModel
+    {
+        get
+        {
+            // Lazily initialize the color view model.
+            if (colorViewModel == null)
+            {
+                colorViewModel = new ColorViewModel(obj);
+            }
+
+            return colorViewModel;
+        }
+    }
+
+    /// <summary>
+    /// Property for controlling the object's rotation about the Y axis.
+    /// </summary>
+    [Binding]
+    public float Rotation
+    {
+        get
+        {
+            return obj.transform.localRotation.eulerAngles.y;
+        }
+        set
+        {
+            obj.transform.localRotation = Quaternion.AngleAxis(value, Vector3.up);
+        }
+    }
+
+    private void Awake()
+    {
+        Assert.IsNotNull(obj);
+    }
+}
